facb04b72226bcef4da65f760a6de2a0f9c2f42f
Ekin <abaliogluekin@gmail.com>
2024-12-31 00:18:12 +0300

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: config => {
@@ -18,9 +16,4 @@ const nextConfig = {
   },
 };
 
-export default withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})(nextConfig);
+export default nextConfig;
